0b5f1a9497c529f9c32ecd4a665e87cb7e9bba96
junghoon <jhyj3636@gmail.com>
2021-02-28 02:56:54 +0900

    프로젝트 생성

---
+++ src/test/java/jh/SimpleBoard/SimpleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package jh.SimpleBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
