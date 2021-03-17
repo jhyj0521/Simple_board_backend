427770f71cbff2d06df82bc01fe3c77e09c656cf
jhyj0521 <jhyj3636@gmail.com>
2021-03-17 14:34:10 +0900

    log 메시지 형식 변경, 인터셉터 추가

---
+++ src/main/java/jh/SimpleBoard/configuration/WebConfiguration.java
@@ -0,0 +1,19 @@
+package jh.SimpleBoard.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Bean
+    public BaseHandlerInterceptor baseHandlerInterceptor() {
+        return new BaseHandlerInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(baseHandlerInterceptor());
+    }
+}
