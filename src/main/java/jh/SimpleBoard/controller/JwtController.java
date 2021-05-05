f442fea2df4f3e810791f76f281c8e5716d247e6
jhyj0521 <jhyj3636@gmail.com>
2021-05-05 18:21:23 +0900

    jwt에 저장된 memberNo 반환하는 api 구현

---
+++ src/main/java/jh/SimpleBoard/controller/JwtController.java
@@ -0,0 +1,26 @@
+package jh.SimpleBoard.controller;
+
+import jh.SimpleBoard.common.BaseResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+import static jh.SimpleBoard.common.CommonUtil.getMemberNo;
+
+@RestController
+@RequestMapping("/jwt")
+public class JwtController {
+    @PostMapping()
+    public BaseResponse getJwtInfo(HttpServletRequest request) {
+        long memberNo = getMemberNo(request);
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("memberNo", memberNo);
+
+        return new BaseResponse(data);
+    }
+}
